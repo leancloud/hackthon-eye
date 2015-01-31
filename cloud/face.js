9dba616d158bfcff12d4a522e4fa12ce73f9200f
wanggc <gwang@avos.com>
2015-01-31 18:18:47 +0800

    face utils

---
+++ cloud/face.js
@@ -0,0 +1,84 @@
+exports.addNewFace = function (picURL, callback) {
+  AV.Cloud.httpRequest({
+    url: 'http://apicn.faceplusplus.com/v2/detection/detect',
+    params: {
+      api_key: 'c825e4c426ac446588a97efcc8023261',
+      api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+      url: picURL,
+    },
+    success: function(httpResponse) {
+      o = JSON.parse(httpResponse.text);
+      f = o.face[0];
+      AV.Cloud.httpRequest({
+        url: 'https://apicn.faceplusplus.com/v2/faceset/add_face',
+        params: {
+          api_key: 'c825e4c426ac446588a97efcc8023261',
+          api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+          face_id: f.face_id,
+          faceset_name: 'eyes',
+        },
+        success: function(httpResponse) {
+          AV.Cloud.httpRequest({
+            url: 'https://apicn.faceplusplus.com/v2/train/search',
+            params: {
+              api_key: 'c825e4c426ac446588a97efcc8023261',
+              api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+              faceset_name: 'eyes',
+            },
+          });
+          callback(
+            'success',
+            {face_id: f.face_id,
+             age: f.attribute.age.value,
+             gender: f.attribute.gender.value}
+          );
+        }
+      });
+    }
+  });
+}
+
+exports.getFaces = function (faceId, callback) {
+  AV.Cloud.httpRequest({
+    url: 'https://apicn.faceplusplus.com/v2/recognition/search',
+    params: {
+      api_key: 'c825e4c426ac446588a97efcc8023261',
+      api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+      key_face_id: faceId,
+      faceset_name: 'eyes',
+      count: 20,
+    },
+    success: function(httpResponse) {
+      o = JSON.parse(httpResponse.text);
+      callback('success', o.candidate);
+    },
+  });
+}
+
+exports.loginByFace = function(picURL, callback) {
+  AV.Cloud.httpRequest({
+    url: 'http://apicn.faceplusplus.com/v2/detection/detect',
+    params: {
+      api_key: 'c825e4c426ac446588a97efcc8023261',
+      api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+      url: picURL,
+    },
+    success: function(httpResponse) {
+      o = JSON.parse(httpResponse.text);
+      faceId = o.face[0].face_id;
+      AV.Cloud.httpRequest({
+        url: 'https://apicn.faceplusplus.com/v2/recognition/search',
+        params: {
+          api_key: 'c825e4c426ac446588a97efcc8023261',
+          api_secret: 'V8ewWd3ZsEaUzGhE45zpcMZj6FeTEEB3',
+          key_face_id: faceId,
+          faceset_name: 'eyes',
+          count: 20,
+        },
+        success: function(resp) {
+          callback('success', JSON.parse(resp.text).candidate[0]);
+        } 
+      });
+    },
+  });
+}
